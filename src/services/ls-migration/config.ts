cf6a7664501a8d4229c8b8e018d4f97e7d27d133
datradito <datradito@gmail.com>
2022-12-05 18:06:15 -0300

    feat: adding migration

--- src/services/ls-migration/config.ts
+++ src/services/ls-migration/config.ts
@@ -1,3 +1,3 @@
-export const IFRAME_HOST = 'https://gnosis-safe.io'
-export const IFRAME_PATH = '/app/migrate.html'
+export const IFRAME_HOST = 'https://safe.evmos.dev'
+export const IFRAME_PATH = '/migrate.html'
 export const MIGRATION_KEY = 'migrationFinished'
