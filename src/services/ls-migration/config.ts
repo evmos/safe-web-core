7b0011fcfab34ec940420ea7f10ae4891940752f
BLAKPC\blak <zhiltsov.nick@gmail.com>
2022-12-26 16:48:44 +0300

    feat: adjust migration script for PRD

--- src/services/ls-migration/config.ts
+++ src/services/ls-migration/config.ts
@@ -1,3 +1,3 @@
-export const IFRAME_HOST = 'https://safe.evmos.dev'
+export const IFRAME_HOST = 'https://safe.evmos.org'
 export const IFRAME_PATH = '/migrate.html'
 export const MIGRATION_KEY = 'migrationFinished'
