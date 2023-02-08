f5b7cdeea550b1ad28498232defb283b14dcbcab
nick8319 <zhiltsov.nick@gmail.com>
2023-02-08 14:52:23 +0300

    fix: cgw url

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,8 +3,8 @@ import chains from './chains'
 export const IS_PRODUCTION = process.env.NEXT_PUBLIC_IS_PRODUCTION
 
 export const GATEWAY_URL_PRODUCTION =
-  process.env.NEXT_PUBLIC_GATEWAY_URL_PRODUCTION || 'https://safe-client.safe.global'
-export const GATEWAY_URL_STAGING = process.env.NEXT_PUBLIC_GATEWAY_URL_STAGING || 'https://safe-client.staging.5afe.dev'
+  process.env.NEXT_PUBLIC_GATEWAY_URL_PRODUCTION || 'https://gateway.safe.evmos.org'
+export const GATEWAY_URL_STAGING = process.env.NEXT_PUBLIC_GATEWAY_URL_STAGING || 'https://gateway.safe.evmos.dev'
 
 // Magic numbers
 export const POLLING_INTERVAL = 15_000
